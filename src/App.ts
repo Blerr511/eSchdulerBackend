7e0a805b17a1476446b0625df35f3d785c380906
Blerr511 <lyovghukasyan@gmail.com>
2021-05-21 02:34:28 +0400

    changed connect type

--- src/App.ts
+++ src/App.ts
@@ -3,17 +3,24 @@ import {Scheduler} from 'helpers/Scheduler';
 
 class App {
 	public init(): void {
+		console.log({
+			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+			privateKey: process.env.FIREBASE_PRIVATE_KEY,
+			projectId: process.env.FIREBASE_PROJECT_ID
+		});
 		this.initAdmin();
 		this.initScheduler();
-
-		console.info('App initialized');
 	}
 
 	private initAdmin(): void {
 		admin.initializeApp({
 			databaseURL: process.env.DB_URL,
 			projectId: 'eshedule',
-			credential: admin.credential.cert(process.env.FIREBASE_CERT_PATH)
+			credential: admin.credential.cert({
+				clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+				privateKey: process.env.FIREBASE_PRIVATE_KEY,
+				projectId: process.env.FIREBASE_PROJECT_ID
+			})
 		});
 	}
 
